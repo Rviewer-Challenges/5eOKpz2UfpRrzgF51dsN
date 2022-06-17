92c3756e6074a39c553cec83f1c19c14d79ade7c
Bryanmartz972 <bryanmartz972@gmail.com>
2022-06-17 16:12:59 -0600

    📦 NEW: Added custom colors to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'blue': '#1D9BF0',
+        'lightblue': '#8ECDF8',
+        'black' : '#0f1419'
+      },
+    },
+    plugins: [],
+  }
 }
