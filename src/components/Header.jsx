ea2f27c9c82153c5f0d564a9c9d9f9e6cd72e061
Bryanmartz972 <bryanmartz972@gmail.com>
2022-06-20 19:01:52 -0600

    👌 IMPROVE: Transitions and Header SparkleIcon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 // Icons
 import Avatar from '@mui/material/Avatar'
-import FlareOutlinedIcon from '@mui/icons-material/FlareOutlined'
+import SparkleIcon from '../assets/images/sparklesIcon.png'
 
 // Image
 import profilePicture from '../assets/images/profile.jpg'
@@ -8,12 +8,12 @@ import profilePicture from '../assets/images/profile.jpg'
 const Header = () => {
 	return (
 		<header className='flex fixed z-10 left-0 top-0 h-[53px] w-full bg-white px-4 items-center'>
-			<div className='mr-[21px]'>
+			<div className='mr-[21px] cursor-pointer'>
 				<Avatar sx={{ width: 32, height: 32 }} src={profilePicture} />
 			</div>
 			<h1 className='font-bold text-black text-[17px] w-[80%]'>Home</h1>
 			<div className='hovershadow hover:bg-black hover:bg-opacity-10'>
-				<FlareOutlinedIcon />
+				<img className='w-5 object-contain scale-x-[-1]' src={SparkleIcon} alt="Top Tweets"/>
 			</div>
 		</header>
 	)
--- src/components/TweetFooter/TweetFooterButton.jsx
+++ src/components/TweetFooter/TweetFooterButton.jsx
@@ -1,7 +1,7 @@
 const TweetFooterButton = ({ children, color, number }) => {
 	return (
-		<div className={`flex items-center hover:cursor-pointer hover:${color}`}>
-			<div className={`hovershadow hover:${color} hover:bg-opacity-10`}>
+		<div className={`flex items-center hover:cursor-pointer hover:${color} transition-all`}>
+			<div className={`hovershadow hover:${color} hover:bg-opacity-10 transition-all`}>
 				{children}
 			</div>
 			<span>{number}</span>
