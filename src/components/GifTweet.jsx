c264c5ecc7f93b355349da6aaff2d315091d611f
Bryanmartz972 <bryanmartz972@gmail.com>
2022-06-26 19:53:45 -0600

    📦 NEW: GifTweet completed, added sample GIF

--- src/components/GifTweet.jsx
+++ src/components/GifTweet.jsx
@@ -1,5 +1,27 @@
-const GifTweet = () => {
-	return <div>GifTweet</div>
+// Image
+import profilePicture from '../assets/images/profile.jpg'
+
+// Components: TweetHeader, TweetFooter
+import TweetFooter from './TweetFooter/TweetFooter'
+import TweetHeader from './TweetHeader'
+import TweetAvatar from './TweetAvatar'
+
+const GifTweet = ({ children, gif }) => {
+	return (
+		<div className='flex w-full px-4 pt-3 border border-b-1 border-black/10 border-x-0 border-t-0 hover:cursor-pointer'>
+			<TweetAvatar picture={profilePicture} />
+			<div className='w-full'>
+				<TweetHeader
+					name={'Bryan Martínez'}
+					username={'thebryanmartinez'}
+					hour={7}
+				/>
+				<div>{!children ? <p>This is a tweet</p> : children}</div>
+				<img className='mt-4 rounded-xl' src={gif} loading='lazy' />
+				<TweetFooter retweets={10000} comments={1000} likes={2} />
+			</div>
+		</div>
+	)
 }
 
 export default GifTweet
